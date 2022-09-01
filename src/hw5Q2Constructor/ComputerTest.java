c9fa045b4073bc23e8081291c0b0a41bc72e8deb
shahriar-mustafa <shahriar.mstfa@gmail.com>
2022-09-01 14:59:44 -0400

    09012022 3:00pm

---
+++ src/hw5Q2Constructor/ComputerTest.java
@@ -0,0 +1,18 @@
+package hw5Q2Constructor;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+		Computer computer = new Computer();
+		Computer computer2 = new Computer("Apple", "Macbook");
+		Computer computer3 = new Computer("MacOS", 1299.99f, 'A', false);
+		
+		// Second outcome
+		System.out.println("------------------------------------------");
+		
+		Computer computer5 = new Computer("Razer", "Blade 15");
+		Computer computer6 = new Computer("Windows 11", 2099.99f, 'A', true);
+
+	}
+
+}
